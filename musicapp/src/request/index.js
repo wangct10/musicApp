3287636062e4daa695c422c92f6f9d5afe746f09
wangct10 <106939085+wangct10@users.noreply.github.com>
2022-06-14 22:23:50 +0800

    Update index.js

--- musicapp/src/request/index.js
+++ musicapp/src/request/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 let service = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://musicappbackground.vercel.app/',
   timeout: 5000
 })
 export default service
